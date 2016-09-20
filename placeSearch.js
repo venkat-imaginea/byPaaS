e745b708b55563908298cfc47d7993d0f4516846
Venkat <venkat.subramanian@imaginea.com>
2016-09-20 14:58:05 +0530

    getGeoCode feature

--- placeSearch.js
+++ placeSearch.js
@@ -4,26 +4,33 @@ var async = require("async");
 
 var GooglePlaces = require("googleplaces");
 var googlePlaces = new GooglePlaces(config.apiKey, config.outputFormat);
-var latLong = [13.082680, 80.270718],
-	searchType = ["restaurant"];
+var geoCode = require('./getBounds.js').geocode;
+// var latLong = [13.082680, 80.270718];
+var latLong = geoCode('Chennai', function(err, res) {
+	if (err) 
+		throw err;
+	var bounds = [res[0].latitude, res[0].longitude];
+	console.log('GeoCode - ', bounds);
+});
+// var searchType = ["restaurant"];
 //var placeId = 'ChIJYTN9T-plUjoRM9RjaAunYW4';
 
 /**
  * Place details requests - https://developers.google.com/maps/documentation/javascript/places#place_search_requests
  */
 
-var parameters = {
-  location: latLong,
-  types: searchType
-};
+// var parameters = {
+//   location: latLong,
+//   types: searchType
+// };
 
-googlePlaces.placeSearch(parameters, function (error, response) {
-	if (error) {
-		console.log(error);
-		throw error;
-	} 
-	console.log('Search Results - ', response.results);	
-	console.log('=======================');
-	console.log('Totally Results fetched - ', response.results.length);
+// googlePlaces.placeSearch(parameters, function (error, response) {
+// 	if (error) {
+// 		console.log(error);
+// 		throw error;
+// 	} 
+// 	console.log('Search Results - ', response.results);	
+// 	console.log('=======================');
+// 	console.log('Totally Results fetched - ', response.results.length);
 	
-});
+// });
