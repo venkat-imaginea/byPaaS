b4f722fdef1d888a102483a75780dae5b42141bf
Venkat <venkat.subramanian@imaginea.com>
2016-09-20 16:36:58 +0530

    file route mistake correction

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var users = require('./routes/getPlaces');
+var getPlaces = require('./routes/getPlaces');
 
 var sweet = require('sweet.js');
 sweet.loadMacro('cspjs');
